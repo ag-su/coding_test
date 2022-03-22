54de3a131f55202cea89a3e47f4d4beb27d26528
silver-eunsu <eunsugim815@gmail.com>
2022-03-22 22:46:08 +0900

    add other problem

--- programmers/level1/음양더하기.py
+++ programmers/level1/음양더하기.py
@@ -1,3 +1,7 @@
 def solution(absolutes, signs):
     lst_num = [num if sign else -1*num for num, sign in zip(absolutes, signs)]        
     return sum(lst_num)
+
+# 내적 (비슷한 코드여서 같은 파일에 작성)
+def solution(a, b):
+    return sum(x*y for x, y in zip(a, b))
