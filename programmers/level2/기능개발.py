892cbcf1441c4ec962dd52845f605ef34f68267e
silver-eunsu <eunsugim815@gmail.com>
2022-04-17 00:39:18 +0900

    add code using stack

--- programmers/level2/기능개발.py
+++ programmers/level2/기능개발.py
@@ -22,3 +22,23 @@ def solution(progresses, speeds):
         answer.append(count)
         
     return answer
+
+
+import math
+def solution(progresses, speeds):
+    answer = []
+    stack = []  
+    
+    for p, s in zip(progresses, speeds): 
+        day = math.ceil((100-p)/s)
+        cnt = 0 
+        while stack and stack[-1] < day and stack[0] < day:  
+            stack.pop()
+            cnt += 1 
+            
+        if cnt > 0:
+            answer.append(cnt)
+    
+        stack.append(day)   
+        
+    return answer + [len(stack)]
